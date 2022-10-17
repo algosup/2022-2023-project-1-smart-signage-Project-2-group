828974e3309f2eae932ac2abf9b870952369976e
Gregory-Pagnoux <114397869+Gregory-Pagnoux@users.noreply.github.com>
2022-10-17 17:43:50 +0200

    update test
    creation of main_test + modif of main

--- go/main.go
+++ go/main.go
@@ -5,17 +5,20 @@ import (
 	"time"
 )
 
-func main() {
+func light_on(val string) string {
 	rate := time.Second / 500
 
 	leds := machine.PA12
 
 	leds.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-	for {
-		leds.High()
-		time.Sleep(rate)
-		leds.Low()
-		time.Sleep(rate)
+	if val == "true" {
+		for {
+			leds.High()
+			time.Sleep(rate)
+			leds.Low()
+			time.Sleep(rate)
+		}
+		return val
 	}
 }
--- go/main_test.go
+++ go/main_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLight_on(t *testing.T) {
+	t.Run("true", func(t *testing.T) {
+		val := "true"
+		got := light_on(val)
+		if got != "true" {
+			fmt.Println("The light isn't turn on")
+		}
+	})
+}
+
+// func TestLight_on(t *testing.T) {
+// 	t.Run("false", func(t *testing.T) {
+// 		val := "false"
+// 		got := TestLight_on(val)
+// 		if got == "false" {
+// 			fmt.Println("The light is turn off")
+// 		}
+// 	})
+// }
