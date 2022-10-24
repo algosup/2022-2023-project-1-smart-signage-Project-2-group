7eab7cff39dc634442a08c1e3ff234ddb96599ff
Gregory-Pagnoux <114397869+Gregory-Pagnoux@users.noreply.github.com>
2022-10-24 22:26:53 +0200

    Update test
    modification test code with Tanguy

--- go/main_test.go
+++ go/main_test.go
@@ -8,7 +8,7 @@ func TestOn(t *testing.T) {
 			on: true,
 		}
 		want := false
-		got := l.on.On()
+		got := l.On()
 		if got != want {
 			t.Errorf("The light isn't turn on")
 		}
@@ -17,6 +17,9 @@ func TestOn(t *testing.T) {
 
 // func TestSet(t *testing.T) {
 // 	t.Run("false", func(t *testing.T) {
+// 		l := LED{
+// 			on: true
+// 		}
 // 		want := true
 // 		got := l.Set(false)
 // 		if got != want {
@@ -33,4 +36,4 @@ func TestOn(t *testing.T) {
 // 			t.Errorf("The light isn't turn on")
 // 		}
 // 	})
-// }
+// }HELLO
